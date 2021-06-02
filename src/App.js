da2057612871fd509bf2f22e1d0abb7b3716f62b
jahhon123 <rasuljonov.jahongir@mail.ru>
2021-06-03 00:51:54 +0900

    vazifa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'; 
 import './App.css';
 
 function App() {
+  const [secondaryMode, setSecondaryMode] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div>
+   
+
+        <div className="App">
+        <div className={secondaryMode ? 'blue' : 'light-mode'}>
+        <div className='container'>
+          <span style={{ color: secondaryMode ? 'grey' : 'yellow'}}>*</span>
+          <div className='switch-checkbox'>
+            <label className='switch'>
+            <input type='checkbox' onChange={() => setSecondaryMode(!secondaryMode)}></input>
+            <span className='slider round'></span>
+
+            </label>
+          </div>
+          <span style={{color:secondaryMode ? '#c96dfd' : 'grey'}}>-</span>
+
+        </div>
+      </div>
+          <h1>Welkome Back</h1>
+          <p>We missed you! login to get Started</p>
+       </div>
+  
+
+  <div className='box'>
+<h2>Login</h2> 
+
+ <button className='login' placeholder='Your email'> Your email <i class="fa fa-user"></i> </button> 
+<div className='jahon'>
+<button className='pasword' placeholder='password'>password <i class="fa fa-lock"></i> </button>
+ </div>
+<input className='chek' type='checkbox'></input> Remember Me <button className='sign'>Sign up</button>
+
+<h4 className='bek'>Don't have an account? <a>Register instaed</a></h4>
+</div>
     </div>
+  
   );
 }
 
